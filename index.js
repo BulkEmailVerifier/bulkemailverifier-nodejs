f2d76c3474a4243b7487f3828aa5181269ca204d
proweb7 <45687254+proweb7@users.noreply.github.com>
2023-07-03 13:17:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const { BouncelessOne, BouncelessBulk } = require('./bounceless');
+const { BouncelessOne, BouncelessBulk } = require('./bouncelessAPI');
+
+const verifySingleEmail = async () => {
+  const bouncelessOne = new BouncelessOne('YOUR_KEY', 'test@email.com');
+  const response = await bouncelessOne.control();
+  console.log(response);
+};
+
+const verifyBulkEmails = async () => {
+  const bouncelessBulk = new BouncelessBulk('YOUR_KEY', 'path/to/file.csv');
+  const response = await bouncelessBulk.upload();
+  console.log(response);
+};
+
+const getBulkInfo = async () => {
+  const bouncelessBulk = new BouncelessBulk('YOUR_KEY', 'FILE_ID');
+  const response = await bouncelessBulk.get_info();
+  console.log(response);
+};
+
+verifySingleEmail();
+verifyBulkEmails();
+getBulkInfo();
